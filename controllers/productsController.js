2c13bb40be3ff964acb13f9ecba583b32d077482
Johan Taborda <johan.david.taborda@gmail.com>
2025-10-31 18:30:44 -0500

    Se agregan asociaciones generales en el archivo index.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,4 @@
-const User = require("../models/User");
-const Products = require("../models/Products");
+const {User, Products} = require("../models") //Importamos el modelo User y Products.
 const fs = require('fs'); //Importamos el módulo fs para manejar el sistema de archivos.
 const path = require('path'); //Importamos path para manejar rutas de archivos.
 
